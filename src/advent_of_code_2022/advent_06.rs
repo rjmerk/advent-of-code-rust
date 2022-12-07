de55cf8199c4867996053d822d1741b408ddb5b0
rjmerk <robbertjan.merk@gmail.com>
2022-12-07 08:37:39 +0100

    Solve advent 2022-06 part 2

--- src/advent_of_code_2022/advent_06.rs
+++ src/advent_of_code_2022/advent_06.rs
@@ -3,24 +3,26 @@ use std::fs;
 pub fn solve()
 {
     let input = fs::read_to_string("data/advent_of_code_2022/input_06.txt").unwrap();
-    let result = first_marker_after_character_n(&input);
-    println!("Nr/ of characters needed to be processed before the first start-of-packet marker is detected is {}", result);
+    let result = first_marker_after_character_n(4, &input);
+    println!("Nr/ of characters needed to be processed before the first start-of-packet marker after 4 uniques is detected is {}", result);
+    let result_2 = first_marker_after_character_n(14, &input);
+    println!("Nr/ of characters needed to be processed before the first start-of-packet marker after 14 uniques is detected is {}", result_2);
 }
 
-fn first_marker_after_character_n(stream: &str) -> usize
+fn first_marker_after_character_n(n:usize, stream: &str) -> usize
 {
-    let mut last_4_chars: Vec<char> = vec![];
+    let mut last_n_chars: Vec<char> = vec![];
     for (i, c) in stream.chars().enumerate() {
-        last_4_chars.push(c);
-        if last_4_chars.len() <= 4 {
+        last_n_chars.push(c);
+        if last_n_chars.len() <= n {
             continue;
         }
-        last_4_chars.remove(0);
-        if all_unique(&last_4_chars) {
+        last_n_chars.remove(0);
+        if all_unique(&last_n_chars) {
             return i + 1;
         }
     }
-    0 // thus
+    stream.len() // this should never happen
 }
 
 fn all_unique(chars: &Vec<char>) -> bool
@@ -43,11 +45,14 @@ mod tests
     #[test]
     fn test_first_marker_after_character_n()
     {
-        assert_eq!(first_marker_after_character_n("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
-        assert_eq!(first_marker_after_character_n("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
-        assert_eq!(first_marker_after_character_n("nppdvjthqldpwncqszvftbrmjlhg"), 6);
-        assert_eq!(first_marker_after_character_n("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
-        assert_eq!(first_marker_after_character_n("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+        assert_eq!(first_marker_after_character_n(4,"mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(first_marker_after_character_n(4,"bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(first_marker_after_character_n(4,"nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(first_marker_after_character_n(4,"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(first_marker_after_character_n(4,"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+        assert_eq!(first_marker_after_character_n(14,"mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(first_marker_after_character_n(14,"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+
     }
 
     #[test]
