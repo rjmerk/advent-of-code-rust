ec8eb14d69a8325256de23693f5cdd7af71c3675
rjmerk <robbertjan.merk@gmail.com>
2023-06-26 11:51:08 +0200

    Solve advent 2022-12 part 2

--- src/advent_of_code_2022/advent_12.rs
+++ src/advent_of_code_2022/advent_12.rs
@@ -5,16 +5,18 @@ use std::collections::VecDeque;
 const A_VALUE: i32 = 'a' as i32;
 const START_CHAR: char = 'S';
 const FINISH_CHAR: char = 'E';
+const EXAMPLE_FILENAME: &str  = "data/advent_of_code_2022/example_12.txt";
 
-pub fn solve()
-{
+pub fn solve() {
     let input = fs::read_to_string("data/advent_of_code_2022/input_12.txt").unwrap();
     let heightmap = Heightmap::from_str(&input);
-    let result = heightmap.number_of_steps();
+    let result = heightmap.number_of_steps_from_start_to_end();
     println!(
         "The fewest steps required to move from your current position to the \
          location that should get the best signal is {}", result);
-
+    let result_2 = heightmap.min_nr_steps_from_any_a_to_end();
+    println!("The fewest steps required to move starting from any square with \
+    elevation a to the location that should get the best signal is {}", result_2);
 }
 
 #[derive(PartialEq, Debug, Hash, Eq, Clone)]
@@ -38,8 +40,7 @@ struct Heightmap {
 }
 
 impl Heightmap {
-    fn from_str(input: &str) -> Self
-    {
+    fn from_str(input: &str) -> Self {
         let rows: Vec<&str> = input.split("\n").collect();
 
         let max_x = rows[0].len();
@@ -62,8 +63,7 @@ impl Heightmap {
         }
     }
 
-    fn find_start_and_end_pos(input: &str, max_x: usize) -> (Point, Point)
-    {
+    fn find_start_and_end_pos(input: &str, max_x: usize) -> (Point, Point) {
         let mut start_pos: Point = Point{x: 0, y:0};
         let mut end_pos: Point = Point{x: 0, y:0};
         for (i, c) in input.replace("\n", "").chars().enumerate() {
@@ -76,14 +76,12 @@ impl Heightmap {
         (start_pos, end_pos)
     }
 
-    fn height_at(&self, x: usize, y: usize) -> i32
-    {
+    fn height_at(&self, x: usize, y: usize) -> i32 {
         let index = y * self.max_x + x;
         self.heights[index]
     }
 
-    fn reachable_points_from(&self, point: &Point) -> Vec<Point>
-    {
+    fn reachable_points_from(&self, point: &Point) -> Vec<Point> {
         let height = self.height_at(point.x, point.y);
         let mut result: Vec<Point> = Vec::new();
 
@@ -109,13 +107,36 @@ impl Heightmap {
         result
     }
 
-    fn number_of_steps(&self) -> i32 {
+    fn number_of_steps_from_start_to_end(&self) -> i32 {
         let mut visited_points: HashSet<Point> = HashSet::new();
         visited_points.insert(self.start_pos.clone());
         let mut active_points: VecDeque<ActivePoint>= VecDeque::new();
         for point in self.reachable_points_from(&self.start_pos) {
             active_points.push_back(ActivePoint{point, distance: 1 })
         }
+        self.nr_steps(visited_points, active_points)
+    }
+
+    fn min_nr_steps_from_any_a_to_end(&self) -> i32 {
+        let mut visited_points: HashSet<Point> = HashSet::new();
+        let mut active_points: VecDeque<ActivePoint> = VecDeque::new();
+        for x in 0..self.max_x {
+            for y in 0..self.max_y {
+                if self.height_at(x, y) == 0 {
+                    visited_points.insert(Point{x, y});
+                    let ap = ActivePoint{point: Point{x, y}, distance: 0};
+                    active_points.push_back(ap);
+                }
+            }
+        }
+        self.nr_steps(visited_points, active_points)
+    }
+
+    fn nr_steps(
+        &self,
+        mut visited_points: HashSet<Point>,
+        mut active_points: VecDeque<ActivePoint>,
+    ) -> i32 {
         let mut min_distance: i32 = i32::MAX;
         while !active_points.is_empty() {
             let ap = active_points.pop_front().unwrap();
@@ -141,23 +162,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example()
-    {
-        let input = fs::read_to_string("data/advent_of_code_2022/example_12.txt")
+    fn test_example() {
+        let input = fs::read_to_string(EXAMPLE_FILENAME)
             .unwrap();
         let hm = Heightmap::from_str(&input);
 
         assert_eq!(hm.start_pos, Point{x: 0, y: 0});
         assert_eq!(hm.end_pos, Point{x: 5, y: 2});
 
-        let result = hm.number_of_steps();
+        let result = hm.number_of_steps_from_start_to_end();
         assert_eq!(result, 31);
     }
 
     #[test]
-    fn test_reachable_points_from()
-    {
-        let input = fs::read_to_string("data/advent_of_code_2022/example_12.txt")
+    fn test_reachable_points_from() {
+        let input = fs::read_to_string(EXAMPLE_FILENAME)
             .unwrap();
         let hm = Heightmap::from_str(&input);
         println!("max_x {}", hm.max_x);
@@ -176,5 +195,18 @@ mod tests {
             Point{x: 0, y:1},
         ];
         assert_eq!(hm.reachable_points_from(&pos), expected);
+    }
+
+    #[test]
+    fn test_example_2() {
+        let input = fs::read_to_string(EXAMPLE_FILENAME)
+            .unwrap();
+        let hm = Heightmap::from_str(&input);
+
+        assert_eq!(hm.start_pos, Point{x: 0, y: 0});
+        assert_eq!(hm.end_pos, Point{x: 5, y: 2});
+
+        let result = hm.min_nr_steps_from_any_a_to_end();
+        assert_eq!(result, 29);
     }
 }
